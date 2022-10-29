6f65d9d7bb78f92cacce314b8175291234ad0fc1
Kherin <kherin.j.s.bundhoo@gmail.com>
2022-10-29 23:31:49 +0400

    refactor: removing irrelevant files

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from "vue";
 import { TroisJSVuePlugin } from "troisjs";
 
 import App from "./App.vue";
-
-import "./assets/main.css";
-
 const app = createApp(App);
 app.mount("#app");
 
